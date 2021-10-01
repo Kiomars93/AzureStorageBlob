38da0561424a0c706e6e33b7700978055393705d
Kio <48633146+Kiomars93@users.noreply.github.com>
2021-10-01 08:54:38 +0200

    Initial commit

---
+++ AzureBlobConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureBlobConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
